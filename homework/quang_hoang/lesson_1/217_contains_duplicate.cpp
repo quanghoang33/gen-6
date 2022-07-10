77beb24dba194ff9a6b77627dbedaa9139f1d054
Hoang Dinh Quang <hoangdinhquang@outlook.com>
2022-07-10 15:28:14 +0800

    Create 217_contains_duplicate.cpp

---
+++ homework/quang_hoang/lesson_1/217_contains_duplicate.cpp
@@ -0,0 +1,13 @@
+// Solution for https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> count;
+        for(int i: nums) {
+            count[i]++;
+            if (count[i] > 1) return true;
+        }
+        return false;
+    }
+};
