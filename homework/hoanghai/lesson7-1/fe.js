115b868bb1c2e0c8651d660502f78e2de90f2345
hainh <hoanghai18497@gmail.com>
2022-08-13 18:57:29 +0700

    bai tap fr 7-1

--- homework/hoanghai/lesson7-1/fe.js
+++ homework/hoanghai/lesson7-1/fe.js
@@ -1,10 +1,32 @@
 // 1. Curring
 // 2. memoize
 // 3. Explain: Javascript asynchronous, single-thread language mean?
+// Javascipt la ngon ngu bat dong Bo , don luong, khi 1 task chay thi se 
+// khong co task khac duoc chay song song voi No , dua tren 1 co che duoc goi la 
+// event loop
+
 // 4. Expain Event Loop
+// Answer: EventLoop la co che javascript xu ly voi chi 1 luong duy nhat:
+// Trong do bao gom cac thanh phan: call stack, heap, queue.
+// Javascript lang nghe cac event, stack la noi duoc thuc this, heap la bo nho tam,
+// call back queue la cho chua 1 so nhung cai dang cho dc dua vao stack
+// Neu nhu stack rong thi no se lay event dau tien trong queue va thuc thi
+
 
 // Guess the output and explain:
 // 5. What's the output?
+// function Person(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+//   const member = new Person('Lydia', 'Hallie');
+
+//   Person.getFullName = function() {
+//     return `${this.firstName} ${this.lastName}`;
+//   };
+
+//   console.log(member.getFullName());
+//  Answer: => Error, you defined Person is a function, not class
 
 // 6: output
 // function sum(a,b) {
