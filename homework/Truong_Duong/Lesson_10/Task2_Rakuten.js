d8c3965c91b1f81837ac498226e1e679d12286cb
Truong <tadashii1417@gmail.com>
2022-09-09 17:35:48 +0700

    improve task 2 rakuten

--- homework/Truong_Duong/Lesson_10/Task2_Rakuten.js
+++ homework/Truong_Duong/Lesson_10/Task2_Rakuten.js
@@ -1,3 +1,6 @@
+// Solution 1: Using Binary Search:
+// Time complexity: O(nlogn)
+
 function isValid(arr, pos) {
   const size = arr.length;
   let maxLeft = arr[0], minRight = arr[size - 1];
@@ -26,5 +29,36 @@ function solution(arr) {
   return res + 1;
 }
 
+// Solution 2: Using prefixSum to save maxValue array from left to right
+// ... and minValue from right to left
+// Time complexity: O(n)
+
+function solution2(arr) {
+  const size = arr.length;
+  const maxLefts = new Array(size);
+  const minRights = new Array(size);
+
+  maxLefts[0] = arr[0];
+  minRights[size - 1] = arr[size - 1];
+
+  for(let i = 1; i < size; i++) {
+    maxLefts[i] = Math.max(maxLefts[i-1], arr[i]);
+  }
+  
+  for(let i = size - 2; i >= 0; i--) {
+    minRights[i] = Math.min(minRights[i+1], arr[i]);
+  }
+
+  for(let i = 0; i < size - 1; i++) {
+    if(maxLefts[i] < minRights[i+1]) {
+      return i + 1;
+    }
+  }
+}
+
+
 console.log(solution([5, -2, 3, 8, 6]) === 3);
 console.log(solution([-5, -5, -5, -42, 6, 12]) === 4);
+
+console.log(solution2([5, -2, 3, 8, 6]) === 3);
+console.log(solution2([-5, -5, -5, -42, 6, 12]) === 4);
