2e200e3bcf753462a2b265d9aec3315c464ff0c2
ThinhNgVhust <45223680+ThinhNgVhust@users.noreply.github.com>
2022-09-25 01:39:57 +0900

    CORS,grab homework from DS lesson1

--- homework/thinhbka/lession10-SD/253. Meeting Rooms II.py
+++ homework/thinhbka/lession10-SD/253. Meeting Rooms II.py
@@ -1,3 +1,4 @@
+#Method1: Using heap
 import heapq as h
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
@@ -10,3 +11,21 @@ class Solution:
                 h.heappop(rooms)
                 h.heappush(rooms,i[1])
         return len(rooms)
+
+#Method2: Using prefix sum
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        maxSize = -1
+        for i in intervals:
+            maxSize = max(maxSize,max(i))
+        ans = [0]*(maxSize+5)
+        for i in intervals:
+            startIdx = i[0]
+            endIdx = i[1]
+            ans[startIdx]+=1
+            ans[endIdx]-=1
+        # print(ans)
+        for i in range(1,maxSize+5):
+            ans[i]+=ans[i-1]
+        # print(ans)
+        return max(ans)
