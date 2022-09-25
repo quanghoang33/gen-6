d0a17456dfee16bb6a8e992594834fab4faeb6fc
ThinhNgVhust <45223680+ThinhNgVhust@users.noreply.github.com>
2022-09-25 15:46:36 +0900

    shortenurl

--- homework/thinhbka/lesson-6-binary-search-bst/1231. Divide Chocolate.py
+++ homework/thinhbka/lesson-6-binary-search-bst/1231. Divide Chocolate.py
@@ -1,3 +1,23 @@
 class Solution:
-    def maximizeSweetness(self, sweetness=[], k=0) -> int:
-        
+    def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
+        def canSplit(arr,value):
+            chunks = 0
+            s = 0
+            for e in arr:
+                s+=e
+                if s >= value:
+                    chunks+=1
+                    s = 0
+                
+            return chunks
+        low = 0
+        high = int(1e9)
+        res = -1
+        while low <= high:
+            mid = (low+high)>>1
+            if canSplit(sweetness,mid) >= k+1:
+                res = mid
+                low = mid+1
+            else:
+                high = mid-1
+        return res
